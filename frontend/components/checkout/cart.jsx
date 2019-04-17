24b8dfb505102df945ce75698b18bcf2490c085a
valery-nguyen <valery.nguyen@ucla.edu>
2019-04-17 19:15:18 -0400

    implemented fix on product search

--- frontend/components/checkout/cart.jsx
+++ frontend/components/checkout/cart.jsx
@@ -19,7 +19,6 @@ class CartPanel extends React.Component {
     if (loading) return null;
     const currentUser = this.props.currentUser;
     const productsInCart = this.props.cartItems;
-    const numProducts = productsInCart.length;
 
     let subtotal = 0;
     productsInCart.forEach(product => {
@@ -42,32 +41,35 @@ class CartPanel extends React.Component {
             <p>Qty: {product.quantity}</p>
             <p>${product.price.toFixed(2)}</p>
           </div>
-          
         </li>
       )
     })
 
+    const emptyCartRendering = (
+      <div className="cart-empty">
+        <h1>Your cart is empty</h1>
+        <div className="cart-checkout-empty-splash"><img src={window.emptyCartURL} /></div>
+      </div>
+    )
+
+    const fullCartRendering = (
+      <div className="cart-full">
+        <div className="cart-header"></div>
+        <div className="cart-body">
+          <button className="cart-checkout-btn" onClick={this.handleCheckoutClick}><h1>checkout</h1></button>
+          <ul>{productsLis}</ul>
+        </div>
+      </div>
+    )
+
     return (
-
-  
       <div className="cart-outer" >
-
         <section className="cart-section">
           <div className="cart-outer">
-            <div className="cart-header">
-              {(productsInCart.length === 0) ? '' : <div className="cart-icon-count">{numProducts}</div>}
-              {(productsInCart.length === 0) ? '' : <i className="cart-header-icon"></i>}
-              {(productsInCart.length === 0) ? <h1>Your cart is empty</h1> : <h2>Subtotal: ${subtotal.toFixed(2)}</h2>}
-            </div>
-            <div className="cart-body">
-              {(productsInCart.length === 0) ? <div className="cart-checkout-empty-splash"><img src={window.emptyCartURL}/></div> : ''}
-
-              {(productsInCart.length === 0) ? '' : <button className="cart-checkout-btn" onClick={this.handleCheckoutClick}><h1>checkout</h1></button>}
-              {(productsInCart.length === 0) ? '' : <ul>{productsLis}</ul>}
-            </div>
+            {(productsInCart.length === 0) ? <div className="cart-empty-container">{emptyCartRendering}</div> : ''}
+            {(productsInCart.length === 0) ? '' : <div className="cart-full-container">{fullCartRendering}</div>}
           </div>
         </section>
-
       </div>
     )
   }
