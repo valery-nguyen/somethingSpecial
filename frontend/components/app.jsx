65be2247d643a80a2076efa43ac0c8dd04dea905
valery-nguyen <valery.nguyen@ucla.edu>
2019-04-19 05:38:20 -0400

    reorganized components - part 9

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -12,7 +12,7 @@ import SessionPanel from './session_form/session_panel_container';
 import CartPanel from './checkout/cart_panel_container';
 import Cart from './checkout/cart_container';
 import ShipPanel from './session_form/ship_panel_container';
-// import ProductsIndexContainer from './../components/products/products_index_container';
+import ProductsIndexContainer from './../components/products/products_index_container';
 import ProductShowContainer from './../components/products/product_show_container';
 // import ProductSearchContainer from './../components/products/product_search_container';
 
@@ -20,11 +20,11 @@ const App = () => (
   <div>
     {/* <Header /> */}
     <Switch>
-      {/* <Route exact path="/products" component={ProductsIndexContainer} /> */}
+      <Route exact path="/products" component={ProductsIndexContainer} />
       <Route exact path="/product/:productTitle" component={ProductShowContainer} />
       <Route exact path="/checkout/cart" component={Cart} />
       {/* <Route path="/search" component={ProductSearchContainer} /> */}
-      {/* <Route exact path="/" component={ProductsIndexContainer} /> */}
+      <Route exact path="/" component={ProductsIndexContainer} />
       <Redirect from='/' to='/' />
     </Switch>
     <Footer />
