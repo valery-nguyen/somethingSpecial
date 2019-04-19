c9d1a281e47774e19689be704b39730629e1f6a4
valery-nguyen <valery.nguyen@ucla.edu>
2019-04-19 04:59:19 -0400

    removed background banner - error fixing attempt

--- frontend/components/header/background_banner.jsx
+++ frontend/components/header/background_banner.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const BackgroundBanner = () => {
   return <Link to="/product/personalized-all-heart-bangle-set">
     <div className="header-background-outer">
-      <img className="header-background" src={window.backgroundImgURL} />
+      {/* <img className="header-background" src={window.backgroundImgURL} /> */}
     </div>
     
   </Link>
