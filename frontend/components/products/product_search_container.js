120c113b4bc592df157ac9e8b53d3a6253243007
valery-nguyen <valery.nguyen@ucla.edu>
2019-04-19 06:04:11 -0400

    removed searchQuery from product_search container

--- frontend/components/products/product_search_container.js
+++ frontend/components/products/product_search_container.js
@@ -1,5 +1,4 @@
 import { connect } from 'react-redux';
-// import queryString from 'query-string';
 
 import ProductSearch from './product_search';
 import { requestSearch } from '../../actions/search_actions';
@@ -8,7 +7,6 @@ const mapStateToProps = (state, ownProps) => {
   const loading = state.ui.loading.searchLoading || state.ui.loading.cartLoading;
   return {
     searchProducts: Object.values(state.entities.searches),
-    // searchQuery: queryString.parse(ownProps.location.search).q, 
     loading
   };
 }
