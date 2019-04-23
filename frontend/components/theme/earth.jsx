c74b17ef0b71a47e4f6a1811d04af6fa1faf38e2
valery-nguyen <valery.nguyen@ucla.edu>
2019-04-23 05:09:34 -0400

    updated video left spacing

--- frontend/components/theme/earth.jsx
+++ frontend/components/theme/earth.jsx
@@ -4,13 +4,18 @@ const Earth = () => {
   return <div className="earth">
     <div>
         <div>
-          <video autoPlay loop muted>
+          <video autoPlay loop muted playsInline>
               <source type="video/mp4" src={window.earthURL} />
           </video>
         </div>
         <div className="earth-links">
-          <a href=""><h1>For Earth: 5 Products That Help People &amp; the Planet</h1></a>
+        <a href="https://www.uncommongoods.com/blog/2018/video-5-incredible-products-that-help-people-the-planet/">
+          <h1>For Earth: 5 Products That Help People &amp; the Planet</h1>
+        </a>
+        <form action="https://www.uncommongoods.com/blog/2018/video-5-incredible-products-that-help-people-the-planet/">
           <button>watch the video</button>
+        </form>
+        
         </div>
     </div>
   </div>
